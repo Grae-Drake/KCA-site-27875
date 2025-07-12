d734fa8a705d03721561cb062b3badc8413038af
Grae Drake <graemdrake@gmail.com>
2025-07-11 22:58:34 -0700

    Update [[...slug]].js
    Fix favicons?

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -34,12 +34,12 @@ function Page(props) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 
                 {/* Enhanced Favicon Support */}
-                <link rel="icon" href="/images/favicon/favicon.ico" sizes="any" />
-                <link rel="icon" href="/images/favicon/favicon.svg" type="image/svg+xml" />
-                <link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon.favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
+                <link rel="icon" type="image/png" href="/public/images/favicon/favicon-96x96.png" sizes="96x96" />
+                <link rel="icon" type="image/svg+xml" href="/public/images/favicon/favicon.svg" />
+                <link rel="shortcut icon" href="/public/images//faviconfavicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/public/images/favicon/apple-touch-icon.png" />
                 <link rel="manifest" href="/site.webmanifest" />
+                <meta name="apple-mobile-web-app-title" content="Kitsap Cultural Access" />
                 <meta name="theme-color" content="#f27c21" />
 
             </Head>
